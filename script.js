5002c157c4c6d49ddc8e88ff02fb071cc2c8314f
abe1144 <19290336+abe1144@users.noreply.github.com>
2025-05-03 19:40:11 -0400

    rps-ui finished

--- script.js
+++ script.js
@@ -16,17 +16,25 @@ function getComputerChoice() {
     return choice
 }
 
-// Use input to get human choice
-
-function getHumanChoice() {
-    choice = prompt("(Rock, paper, or scissors?")
-    return choice.toLowerCase()
+function endGame() {
+    btns.forEach(button => button.disabled = true);
+    if (humanScore === computerScore) {
+        outcome = "It's a tie!";
+    } else if (humanScore > computerScore) {
+        outcome = "You win!";
+    } else {
+        outcome = "The CPU wins!";
+    }
+    outcome_div.textContent = outcome;
+    results.appendChild(outcome_div);
 }
 
 // Keep score
-
+let round = 0;
 let humanScore = 0;
 let computerScore = 0;
+let result = '';
+let outcome = '';
 
 // logic to play a single round
 
@@ -38,61 +46,106 @@ function playRound(humanChoice, computerChoice) {
     // check for tie
     if (humanChoice == computerChoice) {
         console.log(result)
-        console.log("It's a tie")
+        outcome = "It's a tie!"
     } else if (humanChoice == "scissors") {
         if (computerChoice == "paper") {
             humanScore++;
             console.log(result);
-            console.log("You win!");
+            // console.log("You win!");
+            // outcome ="You win!";
+            
         } else {
             console.log(result);
-            console.log("You Lose!");
+            // console.log("You Lose!");
+            // outcome = "You lose!";
             computerScore++;
         }
     } else if (humanChoice == "paper") {
         if (computerChoice == "rock") {
             humanScore++;
             console.log(result);
-            console.log("You win!");
+            // console.log("You win!");
+            // outcome = "You win!";
         } else {
             computerScore++;
             console.log(result);
-            console.log("You Lose!");
+            // console.log("You Lose!");
+            outcome = "You lose!";
         }
     } else if (humanChoice == "rock") {
         if (computerChoice == "scissors") {
             humanScore++;
             console.log(result);
-            console.log("You win!");
+            // console.log("You win!");
+            // outcome = "You win!";
         } else {
             computerScore++;
             console.log(result);
-            console.log("You Lose!");
+            // console.log("You Lose!");
+            // outcome = "You lose!";
         }
     }
     
-
 } 
 
-let round = 1;
+// set up button const and event listeners
 
-while (round <=5) {
+const rock_btn = document.querySelector('#rock');
+const paper_btn = document.querySelector('#paper');
+const scissor_btn = document.querySelector('#scissor');
+const btns = document.querySelectorAll('button');
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+// event listeners
 
-    playRound(humanSelection, computerSelection);
-    round++;
-}
+rock_btn.addEventListener('click', () => {
+    playRound(rock_btn.innerText.toLowerCase(),getComputerChoice());
+});
+paper_btn.addEventListener('click', ()=> {
+    playRound(paper_btn.innerText.toLowerCase(),getComputerChoice());
+});
+scissor_btn.addEventListener('click', ()=>{
+    playRound(scissor_btn.innerText.toLowerCase(),getComputerChoice());
+});
+
+// Dom Variables
+const choices_div = document.createElement('p');
+const round_div = document.createElement('p');
+const outcome_div = document.createElement('p');
+const scores = document.createElement('p');
+// dom to select results div that contains game information
+const results = document.querySelector('.results');
 
 
-if (humanScore > computerScore) {
-    console.log("Human Score: " + humanScore)
-    console.log("Computer Score: " + computerScore)
-    console.log("You win the game!")
-
-} else {
-    console.log("Human Score: " + humanScore)
-    console.log("Computer Score: " + computerScore)
-    console.log("You lose the game!")
-}
+// overall button listener
+btns.forEach(button => {
+    button.addEventListener('click', () => {
+        round++
+        if (round >= 5 ) {
+            endGame()
+            round_div.textContent = "Round: " + round;
+            choices_div.textContent = result;
+            // outcome_div.textContent = outcome;
+            scores.textContent = "Your Score: " + humanScore + " CPU Score: " + computerScore;
+            // add the results of the choices to the div
+            results
+            // append round to the results
+            results.appendChild(round_div);
+            results.appendChild(choices_div);
+            results.appendChild(scores);
+        } else {
+            
+            // console.log(round); add the round number into the div element
+            round_div.textContent = "Round: " + round;
+            choices_div.textContent = result;
+            // outcome_div.textContent = outcome;
+            scores.textContent = "Your Score: " + humanScore + " CPU Score: " + computerScore;
+            // add the results of the choices to the div
+            results
+            // append round to the results
+            results.appendChild(round_div);
+            results.appendChild(choices_div);
+            results.appendChild(scores);
+        }
+        
+    });
+});
